e7e6b92eb9a69d81eb317b62edf0693f18f41508
unknown <deboraccamposs@gmail.com>
2025-12-05 20:53:08 -0300

    Projeto de Conta Bancaria usando Typescript

--- Exercício 5.3/1_salario.js
+++
@@ -1,9 +0,0 @@
-const readline = require("readline-sync");
-
-let salario = readline.questionInt("Digite o salario: ");
-console.log("Salario informado: ", salario);
-let abono = readline.questionInt("Digite o abono: ");
-console.log("Abono informado: ", abono);
-
-let novoSalario = salario + abono;
-console.log("A soma do salario + abono é de: ", novoSalario);
--- README.md
+++
@@ -1,2 +0,0 @@
-# Generation-Brasil---Bootcamp-Javascript-
-Repositório contendo exercícios, práticas de lógica, projetos e atividades realizadas durante o Bootcamp JavaScript da Generation Brasil.
